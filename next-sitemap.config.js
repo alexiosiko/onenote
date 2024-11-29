a7dc6a6dfbf1d1c1d64945a3b489d534b8590cfc
Alexi <jemimassyrup@gmail.com>
2024-11-28 19:12:05 -0800

    sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	siteUrl: 'https://yourdomain.com', // Replace with your website URL
+	generateRobotsTxt: true, // Will generate robots.txt file automatically
+	changefreq: 'daily',
+	priority: 0.7,
+	sitemapSize: 5000,
+	exclude: ['/private-page'], // Add any routes you want to exclude
+  };
+  
