c8bfe1825373bb86c1cb9366f0c21faf3abf685c
Alexi <jemimassyrup@gmail.com>
2024-11-28 19:02:29 -0800

    Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react"
 
 const font = Poppins({
 	subsets: ['latin'],
@@ -27,6 +28,7 @@ export default function RootLayout({
 				className={`${font.className} overflow-hidden bg-secondary antialiased`}>
 				{children}
 				<Toaster />
+				<Analytics />
 			</body>
 		</html>
 	</ClerkProvider>
