2091ec360712fdbbf736b74367d9e7be90e2d509
Alexi <jemimassyrup@gmail.com>
2024-11-28 19:25:04 -0800

    PWA

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+	dest: 'public', // Destination folder for service worker and PWA files
+	disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  });
+  
+  const nextConfig = {
 	webpack: (config: any) => {
 	  config.resolve.alias = {
 		...config.resolve.alias,
-		'canvas': false, // Ignore `canvas` during builds
+		canvas: false, // Ignore `canvas` during builds
 	  };
 	  return config;
 	},
   };
   
-  module.exports = nextConfig;
+  module.exports = withPWA(nextConfig);
   
